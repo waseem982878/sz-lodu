73dbcfae363138cb404cb0a3302ee371f3df4008
Waseem Akram <waseem982878@gmail.com>
2025-09-06 02:30:59 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: Invalid src prop

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.dicebear.com',
+        port: '',
+        pathname: '/8.x/initials/svg',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/v0/b/ludo-king-battles.appspot.com/**',
+      },
+       {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
