53ae71550e98e6723a93603610399b51d547e617
Waseem Akram <waseem982878@gmail.com>
2025-09-06 15:00:22 +0000

    Terminal Mein command chalao Aur meri site ko fire bechkar app hosting p

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'api.dicebear.com',
-        port: '',
-        pathname: '/8.x/initials/svg',
-      },
-      {
-        protocol: 'https',
-        hostname: 'firebasestorage.googleapis.com',
-        port: '',
-        pathname: '/v0/b/ludo-king-battles.appspot.com/**',
-      },
-       {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'api.dicebear.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'firebasestorage.googleapis.com',
+            },
+             {
+                protocol: 'https',
+                hostname: 'picsum.photos',
+            }
+        ]
+    }
 };
 
 export default nextConfig;
