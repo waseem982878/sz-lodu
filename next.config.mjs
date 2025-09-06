9a69f9160c8a7426fd7333f716ca657cf9365857
Waseem Akram <waseem982878@gmail.com>
2025-09-06 02:28:49 +0000

    Try fixing this error: `Unhandled Runtime Error: ChunkLoadError: Loading

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,6 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Required for static export on GitHub Pages
-  output: 'export',
-  
-  // The name of your repository
-  basePath: "/ludo-king-battles",
-  assetPrefix: "/ludo-king-battles/",
-  
-  images: {
-    // Required for static export
-    unoptimized: true,
-  },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
