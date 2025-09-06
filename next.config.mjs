f6a145798092c7c5f4e17332407fba3131387ed9
Waseem Akram <waseem982878@gmail.com>
2025-09-06 02:13:17 +0000

    Github Pages per Meri site live Nahin Ho Rahi Hai Iski kya vajah hai

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,15 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Required for static export on GitHub Pages
   output: 'export',
+  
+  // The name of your repository
+  basePath: "/ludo-king-battles",
+  assetPrefix: "/ludo-king-battles/",
+  
   images: {
+    // Required for static export
     unoptimized: true,
   },
 };
