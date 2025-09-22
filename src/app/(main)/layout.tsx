7829e5e5bf7245fe8c328875fc29669c8eee46f2
Waseem Akram <waseem982878@gmail.com>
2025-09-22 09:39:27 +0000

    Homepage for AAP NE Hyder Kohata Diya Hai. Add a header. Call home page.

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,27 @@
+
+import { ReactNode } from "react";
+import { Header } from "@/components/header";
+import { SidebarProvider } from "@/contexts/sidebar-context";
+import { Sidebar } from "@/components/sidebar";
+import { BottomNav } from "@/components/bottom-nav";
+
+export default function MainLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return (
+    <SidebarProvider>
+        <div className="flex flex-col min-h-screen">
+          <Header />
+          <Sidebar />
+          <main className="flex-1 container mx-auto py-4 px-4 sm:px-6 lg:px-8">
+            {children}
+          </main>
+          {/* Add padding to the bottom to account for the fixed nav bar */}
+          <div className="pb-20"></div>
+          <BottomNav />
+        </div>
+    </SidebarProvider>
+  );
+}
