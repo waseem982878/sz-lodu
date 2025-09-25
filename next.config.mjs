2face13f58c38886d863ab668021b52f50dea780
Waseem Akram <waseem982878@gmail.com>
2025-09-25 23:22:46 +0000

    Unhandled Runtime Error ChunkLoadError: Loading chunk app/landing/page f

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'api.dicebear.com',
-            },
-            {
-                protocol: 'https',
-                hostname: 'picsum.photos',
-            },
-            {
-                protocol: 'https',
-                hostname: 'firebasestorage.googleapis.com',
-            }
-        ],
-    },
-    webpack: (config) => {
-        // This is to fix a bug with recharts and Next.js App Router
-        config.externals.push('recharts');
-        return config;
-    }
+  // Your Next.js configuration options can go here.
+  // For now, even an empty config can help stabilize behavior.
 };
 
 export default nextConfig;
